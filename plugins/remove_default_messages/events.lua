2025288c918dd4f7dee4c29dde780061de71a7cd
Andrei Sebastian <andreieurt@gmail.com>
2024-09-09 21:01:40 +0300

    update(src): blu please do it better

--- plugins/remove_default_messages/events.lua
+++ plugins/remove_default_messages/events.lua
@@ -1,26 +1,28 @@
+local filterMessages = {}
+
+AddEventHandler("OnPluginStart", function()
+	local list = config:Fetch("remove_default_messages.MessageList")
+	for i=1,#list do
+		filterMessages["#"..list[i]] = true
+	end
+end)
+
 AddEventHandler("OnRoundStart", function()
     if config:Fetch("remove_default_messages.DisableGrenadeRadio") then
         server:Execute("sv_ignoregrenaderadio true")
     end
-
 end)
-
 
 AddEventHandler("OnUserMessageSend", function(event, playerid, um, isreliable)
     local user = GetUserMessage(um)
+    local umname = user:GetMessageName()
 
-    if config:Fetch("remove_default_messages.RemoveBlood") then
-        if GetUserMessage(um):GetMessageName():find("CMsgTEBloodStream") then return EventResult.Stop end
-        if GetUserMessage(um):GetMessageName():find("CMsgTEWorldDecal") then return EventResult.Stop end
-    end
-
-    if user:GetMessageName():find("TextMsg") then
-        local messages = config:Fetch("remove_default_messages.MessageList")
-        for i = 1, #messages do
-            if user:GetRepeatedString("param", 0):find(messages[i], 1, true) then
-                return EventResult.Stop
-            end
-        end
+    if umname:find("CMsgTEBloodStream") or umname:find("CMsgTEWorldDecal") then return EventResult.Stop end
+    if umname:find("TextMsg") then
+        local msg = user:GetRepeatedString("param", 0)
+        if filterMessages[msg] then
+			return EventResult.Stop
+		end
     end
 end)
 
